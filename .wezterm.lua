f996e399ec30ba3833f0d0906c398feb1abddbfb
Sambeg Shrestha <sambeg.shrestha@konnectcraft.com>
2024-12-19 07:51:59 +0545

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Tokyo Night (Gogh)'
+-- config.color_scheme = 'Tokyo Night Day'
+
+config.window_decorations = "RESIZE"
+
+return config
