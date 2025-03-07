7e0f4fd0f6cad99a64364afe57672c79518923ab
Sambeg <sthasam2@gmail.com>
2025-03-07 22:10:20 +0545

    feat: add config for tab, add zellij

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,15 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Tokyo Night (Gogh)'
+config.color_scheme = "Tokyo Night (Gogh)"
 -- config.color_scheme = 'Tokyo Night Day'
 
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
+tab_bar_at_bottom = true
 
 return config
